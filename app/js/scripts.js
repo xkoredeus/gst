ae0193ca9a2448eff015076191a07e25276cb2f2
xkored <xkored@gmail.com>
2019-07-10 09:40:37 +0400

    created clients sect and about

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -6,27 +6,34 @@ $(function() {
       dots: true,
       smartSpeed: 700,
       navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
-      // responsive : {
-      //   0   : {
-      //       items: 1
-      //   },
-      //   380 : {
-      //       items: 1
-      //   },
-      //   480 : {
-      //       items: 1
-      //   },
-      //   768 : {
-      //       items: 3
-      //   },
-      //   1040 : {
-      //       items: 4
-      //   }
-      // }
 	});
+  $('.clients__in').owlCarousel({
+    nav: true,
+    loop: true,
+    smartSpeed: 700,
+    dots: false,
+    navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+    responsive : {
+      0   : {
+          items: 1,
+      },
+      380 : {
+          items: 1,
+      },
+      480 : {
+          items: 2,
+      },
+      768 : {
+          items: 3,
+      },
+      1040 : {
+          items: 5,
+      }
+    },
+ });
 	// tabs 
   $(document).ready(function () {
-    $(".tabs-content__item:not(:first-child)").hide();
+    $(".tabs__content-item:not(:first-child)").hide();
     $(".tabs__container div.tabs__content-item.active-tab").show();
     $('ul.tabs__list > li').click(function () {
       if (!($(this).hasClass('active'))) {
